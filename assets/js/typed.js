e96a92ef9584d04161fbeee488f276272cecab3d
24Magic <15381087533@163.com>
2019-05-17 01:35:34 +0800

    modify typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,5 +1,5 @@
 var options = {
-  strings: ["<i>a pretty girl</i>", "<i>a lovely girl</i>", "<i>a sexy girl</i>", "<i>a white girl</i>", "<i>a pure girl</i>", "<i>a shining girl</i>"],
+  strings: ["<i>a pretty girl</i>", "<i>a lovely girl</i>", "<i>a sexy girl</i>", "<i>a white girl</i>", "<i>a pure girl</i>", "<i>a sunny girl</i>"],
   typeSpeed: 40,
   backSpeed: 40,
   smartBackspace: true,
