17e3ba19b91681a492ffe53ff53d30700c480fe6
24Magic <15381087533@163.com>
2019-05-17 00:45:04 +0800

    add roughjs for a svg heart

---
+++ assets/js/rough-svg.js
@@ -0,0 +1,20 @@
+console.log(jQuery)
+console.log(rough)
+    //var roughjs = require('roughjs')
+var roughSvg = (function(rough) {
+    return {
+        initSvg: function() {
+            const heart = document.getElementById('heart')
+            const rc = rough.svg(heart);
+            let node = rc.path(`M150 109
+           A 60 70 30 1 1 247 176
+           A 350 350 0 0 1 150 260
+           A 350 350 0 0 1 53 176
+           A 60 70 -30 1 1 150 109`, {
+                fill: 'red'
+            });
+            heart.appendChild(node);
+        }
+    }
+})(rough)
+roughSvg.initSvg()
